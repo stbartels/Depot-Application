d843775ca003afe538f0dcac8afe45aaa2e1591a
stbartels <stbartels12@gmail.com>
2011-02-21 19:27:45 -0600

    Add self.down to combine_items_in_cart

--- db/migrate/20110219211533_combine_items_in_cart.rb
+++ db/migrate/20110219211533_combine_items_in_cart.rb
@@ -17,5 +17,16 @@ class CombineItemsInCart < ActiveRecord::Migration
       end
     end
   end
+  
+  def self.down
+    # split items with quantity > 1 into multiple items
+    LineItem.where("quantity > 1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create :cart_id => line_item.cart_id,
+        :product_id => line_item.product_id, :quantity => 1
+      end
+    end
+  end
 
 end
